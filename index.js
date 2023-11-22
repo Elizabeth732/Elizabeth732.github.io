1cee032586a84a50c526e640e2a4d30adcaafd07
Elizabeth732 <151767380+Elizabeth732@users.noreply.github.com>
2023-11-22 17:12:14 +0100

    Added Website

---
+++ index.js
@@ -0,0 +1,3 @@
+function pagenotfound(){
+    window.alert("Page not found. Please Go To Youtube and check about it. We will reach out within 30 days after we've fixed the error in the website.")
+}
